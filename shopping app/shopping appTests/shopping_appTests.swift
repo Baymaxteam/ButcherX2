3565ed20783981d9013901b8305f4a9e3d89ab0a
Nina <ninila123@gmail.com>
2016-11-28 22:49:30 +0800

    New IOS app ver0.1, 20161128

---
+++ shopping app/shopping appTests/shopping_appTests.swift
@@ -0,0 +1,36 @@
+//
+//  shopping_appTests.swift
+//  shopping appTests
+//
+//  Created by Nina on 2016/11/18.
+//  Copyright © 2016年 Nina. All rights reserved.
+//
+
+import XCTest
+@testable import shopping_app
+
+class shopping_appTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
