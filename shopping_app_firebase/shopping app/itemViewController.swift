37bb327c6ac829a3d506f46d228b703420090ab7
Rita Yang <cbetlanayang@gmail.com>
2016-12-08 22:55:59 +0800

    item stepper

--- shopping_app_firebase/shopping app/itemViewController.swift
+++ shopping_app_firebase/shopping app/itemViewController.swift
@@ -14,6 +14,9 @@ class itemViewController: UIViewController {
     @IBOutlet var itemPriceView:UILabel!
     @IBOutlet var itemdescriptionView:UITextView!
     @IBOutlet var itemNumberLabel:UILabel!
+    @IBOutlet weak var stepper: UIStepper!
+    
+    
     var item:Item!
 
     override func viewDidLoad() {
@@ -22,14 +25,20 @@ class itemViewController: UIViewController {
         // Do any additional setup after loading the view.
         itemIamgeView.image = UIImage(named: item.img)
         itemNameView.text = item.name
-        let theStringValue :String = String(item.price)
-        itemPriceView.text = "Price: "+theStringValue+"/"+item.unit
+        let theStringValue: String = String(item.price)
+        itemPriceView.text = "價格: " + item.unit + theStringValue + "元"
         itemdescriptionView.text = item.description
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        itemNumberLabel.text = "數量: " + Int(sender.value).description
+        item.buynumber = Int(sender.value)
     }
 
     /*
